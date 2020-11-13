c1ea98377380d55a4081db27cd198a69c300f360
manato.kuroda <a7108100@gmail.com>
2020-11-13 17:42:37 +0900

    feat: add smoke

---
+++ js/index.js
@@ -0,0 +1,90 @@
+let scene, camera, cloudParticles = [],
+  composer;
+
+function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.z = 1;
+  camera.rotation.x = 1.16;
+  camera.rotation.y = -0.12;
+  camera.rotation.z = 0.27;
+
+  let ambient = new THREE.AmbientLight('#555555');
+  scene.add(ambient);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  scene.fog = new THREE.FogExp2('#03544E', 0.00001);
+  renderer.setClearColor(scene.fog.color);
+  document.body.appendChild(renderer.domElement);
+
+  let loader = new THREE.TextureLoader();
+  loader.load("images/smoke.png", function(texture) {
+    cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
+    cloudMaterial = new THREE.MeshLambertMaterial({
+      map: texture,
+      transparent: true
+    });
+
+    for (let p = 0; p < 50; p++) {
+      let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+      cloud.position.set(
+        Math.random() * 800 - 400,
+        500,
+        Math.random() * 500 - 500
+      );
+      cloud.rotation.x = 1.16;
+      cloud.rotation.y = -0.12;
+      cloud.rotation.z = Math.random() * 2 * Math.PI;
+      cloud.material.opacity = 0.55;
+      cloudParticles.push(cloud);
+      scene.add(cloud);
+    }
+  });
+  loader.load("images/stars.jpg", function(texture) {
+
+    const textureEffect = new POSTPROCESSING.TextureEffect({
+      blendFunction: POSTPROCESSING.BlendFunction.COLOR_DODGE,
+      texture: texture
+    });
+    textureEffect.blendMode.opacity.value = 0.2;
+
+    const bloomEffect = new POSTPROCESSING.BloomEffect({
+      blendFunction: POSTPROCESSING.BlendFunction.COLOR_DODGE,
+      kernelSize: POSTPROCESSING.KernelSize.SMALL,
+      useLuminanceFilter: true,
+      luminanceThreshold: 0.3,
+      luminanceSmoothing: 0.75
+    });
+    bloomEffect.blendMode.opacity.value = 1.5;
+
+    let effectPass = new POSTPROCESSING.EffectPass(
+      camera,
+      bloomEffect,
+      textureEffect
+    );
+    effectPass.renderToScreen = true;
+
+    composer = new POSTPROCESSING.EffectComposer(renderer);
+    composer.addPass(new POSTPROCESSING.RenderPass(scene, camera));
+    composer.addPass(effectPass);
+
+    window.addEventListener("resize", onWindowResize, false);
+    render();
+  });
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function render() {
+  cloudParticles.forEach(p => {
+    p.rotation.z -= 0.001;
+  });
+  composer.render(0.1);
+  requestAnimationFrame(render);
+}
+init();
